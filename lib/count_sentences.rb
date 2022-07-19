084a30c5b22bdcce2cd98c8eaffbddb58742b153
Spencer Mathews <smathews0906@gmail.com>
2022-07-18 20:49:13 -0500

    commit tree 1b91fc2f16034da49a42e63a4509cdbb4fc5e112

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array1=[]
+    array = self.split(/[.?!]/)
+    array.each do |x|
+      if x.length>1
+      array1.push(x)
+       end
+    end
+    array1.length
   end
 end
